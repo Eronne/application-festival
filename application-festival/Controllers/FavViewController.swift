54d9373486a6d47f805baaa64c7176958de32d53
Circé <circegrand@gmail.com>
2018-01-14 20:07:46 +0100

    CIRCE_DEV : add JSON files

--- application-festival/Controllers/FavViewController.swift
+++ application-festival/Controllers/FavViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class FavViewController: UIViewController {
 
+    @IBOutlet weak var labelName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+		
+		if let path = Bundle.main.path(forResource: "categories", ofType: "json") {
+			do {
+				let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+				do{
+					
+					let json =  try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+
+					let jsonDictionary =  json as! Dictionary<String,Any>
+					
+					//e.g to get person
+					let categoryArr = jsonDictionary["categories"] as! Array<Dictionary<String,Any>>
+					
+					for category in categoryArr {
+						
+						print(category)
+						
+					}
+				}catch let error{
+					
+					print(error.localizedDescription)
+				}
+				
+			} catch let error {
+				print(error.localizedDescription)
+			}
+		} else {
+			print("Invalid filename/path.")
+		}
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +54,7 @@ class FavViewController: UIViewController {
     @IBAction func dismissViewController(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+
     
     /*
     // MARK: - Navigation
