be5df58cfa80bc927bb0be365c1d3aa968cbae33
Eronne <erwann.letue@gmail.com>
2018-02-14 17:21:25 +0100

    Implemented result Page for calendar

---
+++ application-festival/Controllers/Calendar/CalendarResultController.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarResultController.swift
+//  application-festival
+//
+//  Created by Erwann Letue on 14/02/2018.
+//  Copyright © 2018 Gobelins. All rights reserved.
+//
+
+import Foundation
